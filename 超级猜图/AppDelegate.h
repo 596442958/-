5d671eaa60b2a5c98e062975a4d9b9639c5fceda
Miles <596442958@qq.com>
2015-12-18 10:44:27 +0800

    Initial Commit

---
+++ 超级猜图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  超级猜图
+//
+//  Created by Miles on 12/18/15.
+//  Copyright © 2015 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 超级猜图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  超级猜图
+//
+//  Created by Miles on 12/18/15.
+//  Copyright © 2015 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
